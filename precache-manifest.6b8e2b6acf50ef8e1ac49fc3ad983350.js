d2290417c59721fe46d83c8d8e1ac57cfec1e6d7
Hiran Roy <hironsrrijan@gmail.com>
2022-02-25 10:08:58 +0600

    Updates

---
+++ precache-manifest.6b8e2b6acf50ef8e1ac49fc3ad983350.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207f6814b1b517ed9ea02d4055086528",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "2dcb501683a974a81a33",
+    "url": "/covid19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "a6bad1bd6232f6a7e71c",
+    "url": "/covid19-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "2dcb501683a974a81a33",
+    "url": "/covid19-tracker/static/js/2.3768dda7.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid19-tracker/static/js/2.3768dda7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6bad1bd6232f6a7e71c",
+    "url": "/covid19-tracker/static/js/main.be0fad75.chunk.js"
+  },
+  {
+    "revision": "c5535707c668a65b0e80",
+    "url": "/covid19-tracker/static/js/runtime-main.c7a49258.js"
+  }
+]);
